195847a4621817fbe13bfb1131016922c8d95411
Sahil-Vij <104093019+Sahil-Vij@users.noreply.github.com>
2023-01-11 21:54:02 +0530

    Update App.js

--- REACT Assignment/Golf-Event-Handling-Hard-React-master/src/components/App.js
+++ REACT Assignment/Golf-Event-Handling-Hard-React-master/src/components/App.js
@@ -1,84 +1,87 @@
-import React, { Component, useState, useEffect, useCallback } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-
-const App = () => {
-  const [renderBall, setRenderBall] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [ballPosition, setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-  });
-
-  const start = () => {
-    setRenderBall(!renderBall);
-  };
-
-  const keyHandler = (event) => {
-    let ballPositionClone = { ...ballPosition };
-
-    if (event.keyCode === 37) {
-      ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) -
-        5 +
-        "px";
-    } else if (event.keyCode === 39) {
-      ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) +
-        5 +
-        "px";
-    } else if (event.keyCode === 38) {
-      ballPositionClone.top =
-        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) -
-        5 +
-        "px";
-    } else if (event.keyCode === 40) {
-      ballPositionClone.top =
-        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) +
-        5 +
-        "px";
-    }
-    // ballPositionClone.left = (Number(ballPosition.left.slice(0,ballPosition.left.indexOf("p")))+5)+"px";
-    setBallPosition(ballPositionClone);
-    // console.log(ballPosition);
-  };
-
-  useEffect(() => {
-    window.addEventListener("keydown", keyHandler);
-    return () => window.removeEventListener("keydown", keyHandler);
-  });
-
-  const reset = () => {
-    setRenderBall(!renderBall);
-    setBallPosition({ left: "0px", top: "0px" });
-  };
-  const renderChoice = () => {};
-
-  let compo;
-  if (renderBall) {
-    compo = (
-      <div
-        className="ball"
-        style={{ left: ballPosition.left, top: ballPosition.top }}
-      ></div>
-    );
-  } else {
-    compo = (
-      <button onClick={start} className="start">
-        Start
-      </button>
-    );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={time: 0, x: 0, y: 0 ,top:0,left:0,srartTime:0};
+    this.buttonClickHandler=this.buttonClickHandler.bind(this);
+    this.handeleventlistner=this.handeleventlistner.bind(this);
+    this.tick=this.tick.bind(this);
+  }
+  componentDidMount() {
+    
   }
 
-  return (
+  componentWillUnmount() {
+    
+  }
+  buttonClickHandler(){
+    document.addEventListener("keydown",this.handeleventlistner);
+    clearInterval(this.timerID);
+    this.setState({time: 0, x: 0, y: 0 ,top:0,left:0,startTime:Date.now()})
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  tick() {
+    if(!(this.state.x ==250 &&this.state.y==250)){
+      let timePassed= Date.now() - this.state.startTime ;
+      let sec=Math.floor(timePassed/(1000));
+      this.setState({
+      time:sec
+      });
+    }
+  }
+  handeleventlistner(e){
+    let code=e.keyCode;
+    
+    if(code==39 && !(this.state.x ==250 &&this.state.y==250)){
+        this.setState({
+            x: this.state.left+5, y: this.state.top ,top:this.state.top,left:this.state.left+5
+        });
+    }
+    if(code==37  && !(this.state.x ==250 &&this.state.y==250)){
+        
+        this.setState({
+          
+         x: this.state.left-5, y: this.state.top ,top:this.state.top,left:this.state.left-5
+      });
+    }
+    if(code==38 && !(this.state.x ==250 &&this.state.y==250)){
+        this.setState({
+          
+          x: this.state.left, y:this.state.top-5 ,top:this.state.top-5,left:this.state.left
+      });
+    }
+    if(code==40 && !(this.state.x ==250 &&this.state.y==250)){
+        this.setState({
+          
+          x:this.state.left, y: this.state.top+5 ,top:this.state.top+5,left:this.state.left
+      });
+    }
+    if(this.state.x==250 && this.state.y==250){
+      clearInterval(this.timerID);
+      //alert("matched");
+      document.removeEventListener("keydown",this.handeleventlistner);
+    }
+    //console.log(this.state.x, this.state.y);
+
+  }
+  render() {
+    return (
+    <>
     <div className="playground">
-      {compo}
-      <button onClick={reset} className="reset">
-        Reset
-      </button>
-      {renderChoice()}
+    <div className="ball" style={{ position:"absolute",top:this.state.top +"px",
+      left:this.state.left +"px",
+      }}></div>
+      <button className="start" onClick={this.buttonClickHandler} >Start timer</button>
+      <div className="hole" ></div>
+      <div className="heading-timer">{this.state.time}</div>
     </div>
-  );
-};
+    </>
+    );
+  }
+}
 
 export default App;
